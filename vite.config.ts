27e2de31d578eaed89b6c1e6fad5840faef8171d
andrronov <andrew270504cska@gmail.com>
2025-08-07 02:24:59 +0500

    feat-3: add deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-})
+  server: {
+    host: true,
+    allowedHosts: [
+      "localhost",
+      "127.0.0.1",
+      process.env.VITE_ALLOWED_HOST || "",
+    ].filter(Boolean),
+  },
+});
